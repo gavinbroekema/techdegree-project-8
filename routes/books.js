399d4585e258654b6148a2d4848831e5bf3150c4
Gavin Broekema <gavinbroekema@gmail.com>
2023-03-11 11:13:17 -0600

    Error: Delete Operation Hanging

--- routes/books.js
+++ routes/books.js
@@ -15,29 +15,54 @@ function asyncHandler(cb){
   }
 }
 
-/* GET books listing. */
-
+/* GET all book listings. */
 router.get('/', asyncHandler(async (req, res) => {
   const books = await Book.findAll({ order: [["createdAt", "DESC"]]});
   res.render('layout', {data: books});
 }));
 
-router.post('/new', function(req, res, next) {
+
+/* GET create new book form */
+router.get('/new', function(req, res, next) {
   res.render('new-book', { title: 'Express' });
 });
 
+/* POST create new book page */
+router.post('/new', function(req, res, next) {
+  res.render('layout', { title: 'Express' });
+});
+
+/* GET single book listing */
 router.get('/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
-  console.log(book);
+  // console.log(book);
   res.render('book', {data: book});
 }));
 
+/* UPDATE update book info */
 router.post('/:id', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('book', { title: 'Express' });
 });
 
-router.post('/:id/delete', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* Delete book form. */
+router.get("/:id/delete", asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.render("delete", { book, title: "Delete Book" });
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* Delete individual book. */
+router.post('/:id/delete', asyncHandler(async (req ,res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    await book.destroy();
+    res.redirect("/");
+  } else {
+    res.sendStatus(404);
+  }
+}));
 
 module.exports = router;
